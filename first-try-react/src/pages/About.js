58e0d9e06297a5a0afaafc8d3470fbeac25fe266
Gor <momchyan2297@gmail.com>
2021-11-24 20:59:36 +0300

    add todo

---
+++ first-try-react/src/pages/About.js
@@ -0,0 +1,8 @@
+import React, {Fragment} from "react";
+
+export const About = () => (
+   <div className="container">
+       <h1 className="display-4">Приложение</h1>
+       <p className="lead">Версия приложения <strong>0.9</strong></p>
+   </div>
+)
